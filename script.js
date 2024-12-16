a54ca88c7532ea07d894eb0221f0d67e52552969
thnathip23 <thnathip1050@gmail.com>
2024-12-16 09:40:35 +0700

    new test_v2

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 console.log("thnathip");
 console.log("pond");
+
+
+function calculate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num2 !== 0 ? num1 / num2 : 'Error: Division by zero';
+        default:
+            return 'Invalid operator';
+    }
+}
+
+
+const num1 = 10;
+const num2 = 5;
+const operator = '+';
+
+console.log(`Result: ${calculate(num1, num2, operator)}`);
